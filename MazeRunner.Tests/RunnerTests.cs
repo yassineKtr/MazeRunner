3ceb47461abc2ea6e0d63eb39e0d19c4f544cd63
yassine ktiri <yassinektiri@gmail.com>
2022-06-12 01:06:12 +0100

    Added some helper functions

---
+++ MazeRunner.Tests/RunnerTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+
+namespace MazeRunner.Tests
+{
+    public class RunnerTests
+    {
+        [Fact]
+        public void ShouldBeAbleToFindStartCoordinations()
+        {
+            var maze = new int[][]
+            {
+                new int[] { 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 0, 0, 0, 0, 0, 3 },
+                new int[] { 1, 0, 1, 0, 1, 0, 1 },
+                new int[] { 0, 0, 1, 0, 0, 0, 1 },
+                new int[] { 1, 0, 1, 0, 1, 0, 1 },
+                new int[] { 1, 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 2, 1, 0, 1, 0, 1}
+            };
+            var sol = Runner.FindStartCords(maze);
+            Assert.Equal(sol, new int[] { 6, 1 });
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetValuesOfAPlaceInMaze()
+        {
+            var maze = new int[][]
+            {
+                new int[] { 1, 1, 1, 1, 1, 1, 1 },
+                new int[] { 1, 0, 0, 0, 0, 0, 3 },
+                new int[] { 1, 0, 1, 0, 1, 0, 1 },
+                new int[] { 0, 0, 1, 0, 0, 0, 1 },
+                new int[] { 1, 0, 1, 0, 1, 0, 1 },
+                new int[] { 1, 0, 0, 0, 0, 0, 1 },
+                new int[] { 1, 2, 1, 0, 1, 0, 1}
+            };
+            var sol = Runner.GetValue(new int[] {1,6},maze);
+            Assert.Equal(3, sol);
+        }
+    }
+}
---
+++ MazeRunner/Runner.cs
@@ -0,0 +1,37 @@
+﻿namespace MazeRunner
+{
+    public  class Runner
+    {
+        public static Dictionary<string,int[]> Directions = new Dictionary<string, int[]>()
+        {
+            { "N", new int[] { -1, 0 } },
+            { "S", new int[] { 1 , 0 } },
+            { "E", new int[] { 0, 1 } },
+            { "W", new int[] { 0, -1 } }
+        };
+        public static int[] FindStartCords(int[][] maze)
+        {
+            for (int i = 0; i < maze.Length; i++)
+            {
+                for (int j = 0; j < maze[i].Length; j++)
+                {
+                    if (maze[i][j] == 2)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+            return new int[] { 0, 0 };
+        }
+        public static int GetValue(int[] cords, int[][] maze) => maze[cords[0]][cords[1]];
+        
+        
+        public static string Run(int[][] maze , string[] solution)
+        {
+            var startPointCord = FindStartCords(maze);
+            var nextPoint = GetValue(new int[] { startPointCord[0] + Directions[solution[0]][0] , startPointCord[1] + +Directions[solution[0]][1] }, maze);
+            
+            return string.Empty;
+        }
+    }
+}
